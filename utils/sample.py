d7de50293fb7f2518390543f4ae67ac5e185d8b0
Alanna DEVLIN-GENIN <alannagenin@gmail.com>
2023-02-01 21:26:04 +0000

    Create sample df from whole dataset

---
+++ utils/sample.py
@@ -0,0 +1,27 @@
+from csv_io import import_csv, export_csv
+import pandas as pd
+
+
+class Sample:
+    def __init__(self, dataset, size):
+        self.dataset = pd.DataFrame(dataset)
+        self.size = size
+
+    def create_sample(self):
+        self.sample_dataset = self.dataset.sample(
+            n=self.size, replace=True, random_state=1
+        )
+
+    def save_sample_file(self, path, file):
+        export_csv(self.sample_dataset, path, file)
+
+
+if __name__ == "__main__":
+    BUCKET = 'projet-pfe-adress-matching'
+    FILE_KEY_S3 = 'DonneesCompletes.csv'
+
+    # import of the data
+    full_df = import_csv(BUCKET, FILE_KEY_S3)
+    sample = Sample(dataset=full_df, size=1000)
+    sample_df = sample.create_sample()
+    sample.save_sample_file('.', 'sample')
