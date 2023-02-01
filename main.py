6aca0270f8d58f0419b5dd6250b3c22087d816d3
id1707 <camille.le-potier@eleve.ensai.fr>
2023-02-01 22:16:18 +0000

    define rules for tags

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from standardization.tokens import make_tokens, most_frequent_tokens
+from standardization.tokens import make_tokens, most_frequent_tokens, make_tags
 from standardization.import_csv import import_csv
+import pandas as pd
+from random import sample
 
 if __name__ == '__main__':
     BUCKET = 'projet-pfe-adress-matching'
@@ -7,13 +9,19 @@ if __name__ == '__main__':
 
     # import of the data
     df_complet = import_csv(BUCKET, FILE_KEY_S3)
+    remp_file = pd.read_csv('remplacement.csv', sep=",")
+    lib_voie = pd.read_csv('libvoie.csv', sep=",")
+
     df = df_complet.iloc[:, :8]
 
     # extract addresses column
     adresse = df.iloc[:, 0]
     
     # create tokens for the 100 first addresses
-    tokens = make_tokens(adresse[0:1000000])
+    tokens = make_tokens(adresse.sample(1000), remp_file)
     # frequent = most_frequent_tokens(tokens, 300)
-    print(tokens[0:500])
+    # print(tokens[0:500])
     # print(frequent)
+
+    tags = make_tags(tokens, lib_voie)
+    print(tags)
--- standardization/tokens.py
+++ standardization/tokens.py
@@ -4,7 +4,7 @@ import codecs
 import string
 import re
 
-def make_tokens(adresses):
+def make_tokens(adresses, remp_file):
     '''
     make_tokens: split addresses in tokens, 
     transform them in upper case,
@@ -17,7 +17,16 @@ def make_tokens(adresses):
 
     for row in range(nrows):
         if type(adresses.iloc[row]) == str:
-            # print(adresses.iloc[row])
+
+            # replace special characters
+            adresses.iloc[row] = adresses.iloc[row].replace('/', ' ')
+            adresses.iloc[row] = adresses.iloc[row].replace('-', ' ')
+            adresses.iloc[row] = adresses.iloc[row].replace('(', ' ')
+            adresses.iloc[row] = adresses.iloc[row].replace(')', ' ')
+
+            adresses.iloc[row] = adresses.iloc[row].upper()
+            
+
             splited_adress = re.split(',| |;', adresses.iloc[row])
             splited_adress_new = []
             for word in splited_adress:
@@ -38,12 +47,65 @@ def make_tokens(adresses):
                     word_new = word_new.replace('N!', '')
                     if word_new == "N":
                         word_new = word_new.replace('N', '')
+
+                    # replace abreviation
+                    for raw in range(remp_file.shape[0]):
+                        if word_new == remp_file.iloc[raw, 0]:
+                            word_new = remp_file.iloc[raw, 1]
+                            break
                     
                     if word_new != '':
                         splited_adress_new.append(word_new)
 
             splited_adresses.append(splited_adress_new)
     return splited_adresses
+
+def numvoie(adresse, tag, libvoie, sufixes, index):
+    if adresse[0].isdigit() and len(adresse[index]) < 5 and adresse[index] != '000':
+            tag[index] = "NUMVOIE"
+    else:
+        last_pos = 0
+        for elem in adresse[index]:
+            if elem.isdigit():
+                last_pos +=1
+            else:
+                break
+        if last_pos > 0:
+            letter_after_digit = adresse[index][last_pos : len(adresse[index])]
+            if letter_after_digit in sufixes:
+                tag[index] = "NUMVOIE"
+    return tag
+
+
+def make_tags(tokens, libvoie):
+    tags = []
+    sufixes = ['BIS', 'TER', 'QUATER', 'QUINQUIES', 'SEXIES', 'SEPTIES', 
+    'OCTIES', 'NONIES', 'DECIES', 'B', 'T', 'Q', 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    for adresse in tokens:
+        tag = ["INCONNU" for _ in range(len(adresse))]
+
+        # identify numvoie at the beginning of an address
+        # tag = numvoie(adresse, tag, libvoie, sufixes, index=0)
+       
+        # identify libvoie
+        for token in adresse:
+            if token in list(libvoie.type_voie_maj):
+                tag[adresse.index(token)] = "LIBVOIE"
+
+        # identify numvoie before a libvoie
+        for index in range(1, len(tag)):
+            if tag[index] == 'LIBVOIE' or index == 1:
+                if index > 1:
+                    # print(index, adresse[index])
+                tag = numvoie(adresse, tag, libvoie, sufixes, index=index-1)
+
+        # identify suffix
+        for index in range(1, len(adresse)-1):
+            if adresse[index] in sufixes and (tag[index-1] == 'NUMVOIE' or tag[index+1] == 'LIBVOIE'):
+                tag[index] = 'SUFFIXE'
+        
+        tags.append(tag)
+    return list(zip(tokens, tags))
 
 def most_frequent_tokens(splited_adresses, max_top):
     frequent_tokens = {}
